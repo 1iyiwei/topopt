63b34b35f1f4cfebdfad66082946046d7bb70e35
Li-Yi Wei <git@liyiwei.org>
2018-03-18 17:33:39 -0700

    Revert "Fast code with new filter"

--- src/example.py
+++ src/example.py
@@ -1,24 +1,24 @@
 '''
 tester for topology optimization code
 '''
-import time
+
+import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 from loads import HalfBeam
 from constraints import DensityConstraint
-from fesolvers import CooFESolver
+from fesolvers import LilFESolver, CooFESolver
 from topopt import Topopt
 
 if __name__ == "__main__":
-    t = time.time()
     # material properties
     young = 1
     poisson = 0.3
 
     # constraints
     volfrac = 0.4
-    xmin = 1e-9
+    xmin = 0.001
     xmax = 1.0
 
     # input parameters
@@ -35,24 +35,22 @@ if __name__ == "__main__":
     load = HalfBeam(nelx, nely)
 
     # constraints
-    density_constraint = DensityConstraint(volume_frac=volfrac, density_min=xmin, density_max=xmax)
+    density_constraint = DensityConstraint(volume_frac = volfrac, density_min = xmin, density_max = xmax)
 
     # optimizer
     verbose = True
-    fesolver = CooFESolver(verbose=verbose)
-    optimizer = Topopt(fesolver, young, poisson, verbose=verbose)
+    fesolver = CooFESolver(verbose = verbose)
+    optimizer = Topopt(fesolver, young, poisson, verbose = verbose)
 
     # compute
-    history = True
+    history = False
     x = optimizer.init(load, density_constraint)
     x, x_more = optimizer.layout(load, density_constraint, x, penal, rmin, delta, loopy, history)
-
-    print('Elapsed time is: ', time.time() - t, 'seconds.')
 
     if history:
         x_history = x_more
         loop = len(x_history)
-    else:
+    else:    
         loop = x_more
         x_history = None
 
