91e958efb2ab1c4c97fd48d7490123e732e2b433
AJJLagerweij <A.J.J.Lagerweij@student.tudelft.nl>
2018-06-21 11:31:09 +0200

    Fig bug in topopt file when connecting to solver

--- src/topopt.py
+++ src/topopt.py
@@ -3,7 +3,6 @@ topology optimization
 
 2D only for now
 '''
-
 import numpy as np
 import math
 from scipy.ndimage import convolve
@@ -22,38 +21,45 @@ class Topopt(object):
         self.verbose = verbose
 
     # topology optimization
-    def layout(self, load, constraint, x, penal, rmin, delta, loopy, history=False):
+    def layout(self, load, constraint, x, penal, rmin, delta, loopy, filt, history=False):
+        # check if an existing filter was selected
+        if filt != 'sensitivity' and filt != 'density':
+            raise ValueError('No valid filter was selected, density of sensitivity are the only options')
 
         loop = 0  # number of loop iterations
         change = 1.0  # maximum density change from prior iteration
 
         if history:
-            x_history = [x]
+            xf_history = [1-x]
 
         while (change > delta) and (loop < loopy):
             loop = loop + 1
-            x, change, c = self.iter(load, constraint, x, penal, rmin)
+            x, change, c = self.iter(load, constraint, x, penal, rmin, filt)
             if self.verbose: print('It.: {0:4d},  Obj.: {1:8.2f},  ch.: {2:0.3f}'.format(loop, c, change), flush=True)
-            if history: x_history.append(x)
+            if history:
+                xf = self.densityfilt(x, rmin, filt)
+                xf_history.append(1-xf)
+
+        # the filtered density is the physical desity
+        xf = self.densityfilt(x, rmin, filt)
 
         # done
         if history:
-            return x, x_history
+            return xf, xf_history
         else:
-            return x, loop
+            return xf, loop
 
     # initialization
     def init(self, load, constraint):
         (nelx, nely) = load.shape()
         xlist, ylist, values = load.passive()
-        
+
         x = np.ones((nely, nelx))*constraint.volume_frac()
         x[ylist, xlist] = values
         return x
 
     # iteration
-    def iter(self, load, constraint, x, penal, rmin):
-
+    def iter(self, load, constraint, x, penal, rmin, filt):
         xold = x.copy()
 
         # element stiffness matrix
@@ -61,17 +67,20 @@ class Topopt(object):
         ke = self.lk(self.young, self.poisson)
         kmin = self.lk(Emin, self.poisson)
 
-        # displacement via finite element analysis
-        u = self.fesolver.displace(load, x, ke, kmin, penal)
-        
-        # compliance and derivative
-        c, dc = self.comp(load, x, u, ke, penal)
+        # applying the density filter if required
+        xf = self.densityfilt(x, rmin, filt)
 
-        # filter
-        dc = self.filt(x, rmin, dc)
+        # displacement via finite element analysis
+        u = self.fesolver.displace(load, xf, ke, kmin, penal)
+
+        # compliance and derivative
+        c, dc = self.comp(load, xf, u, ke, penal)
+
+        # applying the sensitvity filter if required
+        dcf = self.sensitivityfilt(xf, rmin, dc, filt)
 
         # update
-        x = self.update(constraint, x, dc, load)
+        x = self.update(constraint, x, dcf, load)
 
         # how much has changed?
         change = np.amax(abs(x-xold))
@@ -80,6 +89,8 @@ class Topopt(object):
 
     # updated compliance algorithm
     def comp(self, load, x, u, ke, penal):
+        '''funcion calculates compliance and compliance density derivative'''
+
         nely, nelx = x.shape
         xe = x.T.flatten()  # flat list wich desities
 
@@ -93,27 +104,61 @@ class Topopt(object):
 
         dc = -penal * (xe ** (penal-1)) * ce  # compliance derivative
         dc = dc.reshape((nelx, nely)).T
-       
+
         return c, dc
 
-    # new filter based upon C++ accelerated code
-    def filt(self, x, rmin, dc):
-        rminf = math.floor(rmin)
-        nely, nelx = x.shape
+    # sensitivity filter
+    def densityfilt(self, x, rmin, filt):
+        ''' filters the density with a radius of rmin if:
 
-        # define normalized convolution kernel based upon rmin
-        size = rminf*2+1
-        kernel = np.zeros((size, size))
-        for i in range(size):
-            for j in range(size):
-                dis = np.sqrt((rminf-i)**2 + (rminf-j)**2)
-                kernel[i, j] = np.max((0, rmin - dis))
-        kernel = kernel/np.sum(kernel)  # normalisation
+            >>> filt=='density'  '''
+        if filt == 'density':
+            rminf = math.floor(rmin)
+            nely, nelx = x.shape
 
-        # elementwise multiplication of x and dc
-        xdc = dc*x
-        xdcn = convolve(xdc, kernel, mode='reflect')
-        dcn = np.divide(xdcn, x, out=np.zeros_like(xdcn), where=x!=0)  # fix devision by 0
+            # define normalized convolution kernel based upon rmin
+            size = rminf*2+1
+            kernel = np.zeros((size, size))
+            for i in range(size):
+                for j in range(size):
+                    dis = np.sqrt((rminf-i)**2 + (rminf-j)**2)
+                    kernel[i, j] = np.max((0, rmin - dis))
+            kernel = kernel/np.sum(kernel)  # normalisation
+
+            # apply convolution filter
+            xf = convolve(x, kernel, mode='reflect')
+
+        else:
+            xf = x
+
+        return xf
+
+    # sensitivity filter
+    def sensitivityfilt(self, x, rmin, dc, filt):
+        ''' filters sensitivity with a radius of rmin if:
+
+            >>> filt=='sensitivity'
+        '''
+        if filt == 'sensitivity':
+            rminf = math.floor(rmin)
+            nely, nelx = x.shape
+
+            # define normalized convolution kernel based upon rmin
+            size = rminf*2+1
+            kernel = np.zeros((size, size))
+            for i in range(size):
+                for j in range(size):
+                    dis = np.sqrt((rminf-i)**2 + (rminf-j)**2)
+                    kernel[i, j] = np.max((0, rmin - dis))
+            kernel = kernel/np.sum(kernel)  # normalisation
+
+            # elementwise multiplication of x and dc
+            xdc = dc*x
+            xdcn = convolve(xdc, kernel, mode='reflect')
+            dcn = np.divide(xdcn, x, out=np.zeros_like(xdcn), where=x!=0)  # fix devision by 0
+
+        else:
+            dcn = dc
 
         return dcn
 
@@ -162,4 +207,3 @@ class Topopt(object):
                       [k[6], k[3], k[4], k[1], k[2], k[7], k[0], k[5]],
                       [k[7], k[2], k[1], k[4], k[3], k[6], k[5], k[0]]])
         return ke
-
