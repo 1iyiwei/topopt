adce37be54854eb48e3739e40def726d29eae90d
Bram Lagerweij <bramlagerweij16@gmail.com>
2021-10-19 16:56:03 +0300

    Fixing documentation layout issues.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,7 @@ html_experimental_html5_writer = True
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
